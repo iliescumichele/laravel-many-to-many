c26e0ef8a011324758731ef1babf179c1c0bedbc
iliescumichele <iliescumichele@gmail.com>
2022-07-15 16:49:55 +0200

    Popolato la tabella ponte con elementi random

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         //$this->call(CategoriesTableSeeder::class);
         //$this->call(UpdatePostsSeeder::class);
         //$this->call(TagsTableSeeder::class);
+        $this->call(PostsTagsTableSeeder::class);
     }
 }
